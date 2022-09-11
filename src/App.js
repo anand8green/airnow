a27f1048eafcb97ff000050343083e3efe08d854
Anand Kumar <247devs@gmail.com>
2022-09-11 11:11:42 +0100

    added local json data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import SdkContainer from './Components/SdkContainer'
-
+import installedData from './JSON/installed.json'
+import unInstallData from './JSON/uninstalled.json'
 const App = () => {
   const [install, setInstall] = useState(true)
+  const [instData] = useState(installedData?.data?.installedSdks)
+  const [unInstData] = useState(unInstallData?.data?.uninstalledSdks)
 
   return (
     <div>
@@ -10,9 +13,9 @@ const App = () => {
       <button onClick={() => setInstall(false)}>UnInstalled</button>
 
       {install ? (
-        <SdkContainer title="Installed SDK" />
+        <SdkContainer title="Installed SDK" totalSdk={instData.length} />
       ) : (
-        <SdkContainer title="UnInstalled SDK" />
+        <SdkContainer title="UnInstalled SDK" totalSdk={unInstData.length} />
       )}
     </div>
   )
--- src/Components/SdkContainer.js
+++ src/Components/SdkContainer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const SdkContainer = ({ title }) => {
+const SdkContainer = ({ title, totalSdk }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <h3>{totalSdk}</h3>
     </div>
   )
 }
