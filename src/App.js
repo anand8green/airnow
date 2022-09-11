8d7c070f5cae9df5134bc7a94a13842e3bd7c327
Anand Kumar <247devs@gmail.com>
2022-09-11 11:04:43 +0100

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SdkContainer from './Components/SdkContainer'
 
 const App = () => {
-  return <div>App</div>
+  const [install, setInstall] = useState(true)
+
+  return (
+    <div>
+      <button onClick={() => setInstall(true)}>Installed</button>
+      <button onClick={() => setInstall(false)}>UnInstalled</button>
+
+      {install ? (
+        <SdkContainer title="Installed SDK" />
+      ) : (
+        <SdkContainer title="UnInstalled SDK" />
+      )}
+    </div>
+  )
 }
 
 export default App
